866f97e8e13e408382bfe9cae6e3cf25f732df5f
NominDulal <d27660344@gmail.com>
2025-08-04 19:10:42 +0545

    Question 2 solved

--- function/practice.py
+++ function/practice.py
@@ -4,12 +4,12 @@
 # Takes an integer as input.
 # Prints "Even" if the number is even, otherwise prints "Odd".
 
-def check_even_odd(num):
-    if num%2==0:
-        return "even"
-    else:
-        return "odd"
-print(check_even_odd(4))    
+# def check_even_odd(num):
+#     if num%2==0:
+#         return "even"
+#     else:
+#         return "odd"
+# print(check_even_odd(4))    
 
 # question2 
 # 2. Grade Calculator
@@ -22,3 +22,17 @@ print(check_even_odd(4))
 # D: 60–69
 # F: below 60
 
+def calculate_grade(score):
+    if score>=90 and score<=100:
+        return "A"
+    elif score>=80 and score<=89:
+        return "B"
+    elif score>=70 and score<=79:
+        return "C"
+    elif score>=60 and score<=69:
+        return "D"
+    elif score<60 and score>=0:
+        return "F"
+    else:
+        return "Invalid number"
+print(calculate_grade(0))    
