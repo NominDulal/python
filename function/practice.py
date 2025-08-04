250a77102d26318a3f17ca44f310c03b4715a278
NominDulal <d27660344@gmail.com>
2025-08-04 19:18:54 +0545

    Question 3 solved

--- function/practice.py
+++ function/practice.py
@@ -22,17 +22,33 @@
 # D: 60–69
 # F: below 60
 
-def calculate_grade(score):
-    if score>=90 and score<=100:
-        return "A"
-    elif score>=80 and score<=89:
-        return "B"
-    elif score>=70 and score<=79:
-        return "C"
-    elif score>=60 and score<=69:
-        return "D"
-    elif score<60 and score>=0:
-        return "F"
+# def calculate_grade(score):
+#     if score>=90 and score<=100:
+#         return "A"
+#     elif score>=80 and score<=89:
+#         return "B"
+#     elif score>=70 and score<=79:
+#         return "C"
+#     elif score>=60 and score<=69:
+#         return "D"
+#     elif score<60 and score>=0:
+#         return "F"
+#     else:
+#         return "Invalid number"
+# print(calculate_grade(0))    
+
+# 3. Maximum of Three Numbers
+# Write a function find_max(a, b, c) that:
+# Takes three numbers as arguments.
+# Returns the largest among them using if-else (don’t use max()).
+def find_max(a, b, c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
     else:
-        return "Invalid number"
-print(calculate_grade(0))    
+        return c
+print(find_max(23,24 ,56 )) 
+print(find_max(23,-34 ,0 ))   
+print(find_max(20,20 ,20 ))
+print(find_max(-1,-10000 ,-56 ))
