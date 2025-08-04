6c8a832e3c599813e097abc2b54a308eab06fe2a
NominDulal <d27660344@gmail.com>
2025-08-04 18:55:07 +0545

    updated question2

---
+++ function/practice.py
@@ -0,0 +1,24 @@
+# Question 1 : 
+#1. Even or Odd Checker
+# Write a function called check_even_odd(num) that:
+# Takes an integer as input.
+# Prints "Even" if the number is even, otherwise prints "Odd".
+
+def check_even_odd(num):
+    if num%2==0:
+        return "even"
+    else:
+        return "odd"
+print(check_even_odd(4))    
+
+# question2 
+# 2. Grade Calculator
+# Create a function calculate_grade(score) that:
+# Accepts a number from 0 to 100.
+# Uses if-else statements to return a letter grade:
+# A: 90–100
+# B: 80–89
+# C: 70–79
+# D: 60–69
+# F: below 60
+
