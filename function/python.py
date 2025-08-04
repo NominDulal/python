f2dc39624aba95aadda213de690ee00dcd7e42a7
NominDulal <d27660344@gmail.com>
2025-08-04 18:49:40 +0545

    initializing

---
+++ function/python.py
@@ -0,0 +1,66 @@
+# print("nomin")+print("dulal")
+
+# global variable 
+
+# a=4
+# b=4
+# def apple():
+#     b=5
+#     print(b)
+
+
+# apple()
+# print(b)
+
+# def apple():
+#     print("i am here ")
+#     return 
+#     print("watermelon")
+
+# apple();
+# def a(val):
+#      b=5
+#      return b
+
+# def b(val):
+#      return val
+
+# val1=a(5)
+# val2=b(5)
+
+# def a(val):
+#    print(val)
+#    return val*val
+
+# print(a(10)) 
+
+# def fruit1(a):
+#     return a
+# a=fruit1("Hello")
+# def fruit2(b):
+#     return b
+# b=fruit2(" World")
+# print(a + b)
+# def fruit1(fruit):
+#     return fruit
+# a=fruit1("Apple")
+# b=fruit1(" Mango")
+# print(a + b)
+# a=4
+# if a == 4:
+#     print("Hello")
+# else:
+#     print("Bye")    
+# def num(a):
+#     if a==5:
+#         return "Yes 5"
+#     else:
+#         return "No 5"    
+# print(num(5))
+# print(num(3))
+def check_even_odd(num):
+    if num%2==0:
+        return "even"
+    else:
+        return "odd"
+print(check_even_odd(4))    
